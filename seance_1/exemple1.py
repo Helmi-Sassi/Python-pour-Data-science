c2feb355e9f39c150460e78b42fa9854cfd6dd96
Helmi-Sassi <sassi_helmi@live.fr>
2024-02-01 14:53:48 +0100

    First commit

---
+++ seance_1/exemple1.py
@@ -0,0 +1,7 @@
+nom="Ali" #variable de type str
+age=30   #variable de type int
+salaire=1200,50 #variable de type float
+est_celib=True #variable de type booléan 
+#afficher les informations de la personne
+#on appelle la fonction print
+print(nom)
